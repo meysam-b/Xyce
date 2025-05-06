29a4c21e70e142a249e6fe4c5bd01ff826ad3389
Meysam Bahmanian <meysamb@mail.uni-paderborn.de>
2025-05-06 16:25:33 +0200

    adding dummy parameter mulu0 to support tsmc 65nm technology

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-cmake -B build -D CMAKE_INSTALL_PREFIX=/usr/local/Xyce -D Trilinos_ROOT=/usr/local/XyceLibs/Parallel -G Ninja
-
-
+cmake -B build -D CMAKE_BUILD_TYPE=Debug -DXyce_ADMS_MODELS=ON -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -D CMAKE_INSTALL_PREFIX=~/local/Xyce -D Trilinos_ROOT=/usr/local/XyceLibs/Parallel -G Ninja
